1c5fb9fb018b4f5cb4afb45ba164572917fc1796
karthiksaivinjamarla-jpg <karthiksaivinjamarla@gmail.com>
2026-09-02 15:17:37 +0530

    chore: remove unused map placeholder component

--- src/components/MapLegend.tsx
+++
@@ -1,9 +0,0 @@
-export function MapLegend() {
-  return (
-    <div className="map-legend">
-      <span><i className="legend-observed" /> Observed</span>
-      <span><i className="legend-ai" /> AI forecast</span>
-      <span><i className="legend-band" /> Uncertainty</span>
-    </div>
-  );
-}
